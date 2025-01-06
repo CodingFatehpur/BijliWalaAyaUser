bb0d6ec31a85231e6b7bb378ab5052791d403c1d
Om Maurya <omaurya367@gmail.com>
2025-01-06 18:03:45 +0530

    edited by om

--- app/src/main/java/digi/coders/bijliwalaayauser/Activity/MainActivity.java
+++ app/src/main/java/digi/coders/bijliwalaayauser/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package digi.coders.bijliwalaayauser;
+package digi.coders.bijliwalaayauser.Activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import digi.coders.bijliwalaayauser.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/digi/coders/bijliwalaayauser/Activity/NumberLoginActivity.java
+++ app/src/main/java/digi/coders/bijliwalaayauser/Activity/NumberLoginActivity.java
@@ -15,13 +15,10 @@ public class NumberLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_number_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+//        ...sdfajsfasjdfas
+
     }
-    // editeb uvakdfgbadmfnbqa
 }
